083415a257e9609f24d540ceda0162aa438476fe
robinjac <robin23jac@gmail.com>
2023-02-04 23:49:24 +0100

    Print out ticker json on client

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-    import PriorityList from "../lib/components/PriorityList.svelte";
+    import PriorityList from "$lib/components/PriorityList.svelte";
+    import tickerJson from "$lib/assets/ticker.example.json";
 
-    // your script goes here
+    console.log(tickerJson);
+
 </script>
 
 <h1>Here is where all the awesome work will be done</h1>
