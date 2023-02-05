ef5f68a81bf73aa73b34cc2df879be8741654179
robinjac <robin23jac@gmail.com>
2023-02-05 17:05:28 +0100

    Adjust test script for AV

--- scripts/get_test_data_av.py
+++ scripts/get_test_data_av.py
@@ -1,13 +1,14 @@
 import csv
+import json
 import requests
+from dotenv import load_dotenv
 
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=demo'
+load_dotenv()  # Make sure there is a .env with a ALPHA_VANTAGE_API_KEY before this is run
 
-with requests.Session() as s:
-    download = s.get(CSV_URL)
-    decoded_content = download.content.decode('utf-8')
-    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-    my_list = list(cr)
-    for row in my_list:
-        print(row)
+response = requests.get(  # Response returns in CSV format https://www.alphavantage.co/documentation/#intraday-extended
+    'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey={ALPHA_VANTAGE_API_KEY}')
+
+csv_list = list(csv.reader(response.text.splitlines(), delimiter=','))
+
+with open('src/lib/assets/alpha_vantage_IBM_time_series.json', 'w', encoding='utf-8') as f:
+    json.dump(csv_list, f, ensure_ascii=False, indent=4)
